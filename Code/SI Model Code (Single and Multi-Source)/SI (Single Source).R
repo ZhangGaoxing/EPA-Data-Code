939e6e0989c83f4324979f56c24b80e3cfe6c52b
Zhang Yuexin <zhangyuexin121@live.cn>
2020-07-07 16:01:10 +0800

    .

--- Code/SI Model Code (Single and Multi-Source)/SI (Single Source).R
+++ Code/SI Model Code (Single and Multi-Source)/SI (Single Source).R
@@ -6,10 +6,10 @@ load(file="./Graphs/USPG.RData")
 
 #Code to simulate infection over a graph using SI model.
 
-USPG_Hetero_40=list() #Replace Facebook_Hetero_2 depending on graph and infection size
-USPG_Hetero_40_Time=list()
+USPG_Hetero_20=list() #Replace Facebook_Hetero_2 depending on graph and infection size
+USPG_Hetero_20_Time=list()
 
-k=1
+k=112
 while(k<=200){ #Generate 100 infection graphs
 	print(k)
 random=as.numeric(c((sample(V(graph))[1]))) #Randomly pick one source
@@ -48,7 +48,7 @@ while(1)
 	temp=perm_active
 	#temp_active=list()
 	tt=tt+1
-	#ttt=0
+	ttt=0
 	for(inactive in rel_neighbors)
 	{	
 		count=0
@@ -90,10 +90,10 @@ while(1)
 			prob=unlist(edge_list)
 		}
 
-		#ttt=ttt+1
+		ttt=ttt+1
 		if(prob>runif(n=1, min=0, max=1))
 		{
-			time[length(unlist(perm_active))+1]=tt#as.numeric(paste(tt,ttt,sep="."))
+			time[length(unlist(perm_active))+1]=as.numeric(paste(tt,ttt,sep="."))
 			perm_active[length(unlist(perm_active))+1]=(inactive)
 			
 			#print(unlist(perm_active))				
@@ -103,8 +103,8 @@ while(1)
 	if(length(unlist(perm_active))>=length(V(graph))*0.4) #Set infection size
 			{
 				if(length(unlist(perm_active))<=length(V(graph))*0.42){
-				USPG_Hetero_40[[length(USPG_Hetero_40)+1]]=unlist(perm_active)
-				USPG_Hetero_40_Time[[length(USPG_Hetero_40_Time)+1]]=unlist(time)
+				USPG_Hetero_20[[length(USPG_Hetero_20)+1]]=unlist(perm_active)
+				USPG_Hetero_20_Time[[length(USPG_Hetero_20_Time)+1]]=unlist(time)
 				print(unlist(perm_active))	
 				k=k+1
 				break
@@ -121,5 +121,5 @@ while(1)
 }
 }
 
-save(USPG_Hetero_40, file="./200/USPG_Hetero_40.RData")
-save(USPG_Hetero_40_Time, file="./200/USPG_Hetero_40_Time.RData")
+save(USPG_Hetero_20, file="./200_t/USPG_Hetero_20.RData")
+save(USPG_Hetero_20_Time, file="./200_t/USPG_Hetero_20_Time.RData")
