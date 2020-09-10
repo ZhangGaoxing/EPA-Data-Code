706f9000155fae802031e6744e9f3f36b902e880
Zhang Yuexin <zhangyuexin121@live.cn>
2020-09-10 16:56:20 +0800

    .

--- Code/SI Model Code (Single and Multi-Source)/SI (Single Source).R
+++ Code/SI Model Code (Single and Multi-Source)/SI (Single Source).R
@@ -2,17 +2,17 @@ library(igraph)
 
 #Load the desired graph over which the infection is to be generated
 #Set the working directory
-load(file="./Graphs/Random.RData")
+load(file="./Graphs/USPG.RData")
 
 #Code to simulate infection over a graph using SI model.
 
-Random_Hetero_30=list() #Replace Random_Hetero_2 depending on graph and infection size
-Random_Hetero_30_Time=list()
+USPG_Hetero_30=list() #Replace USPG_Hetero_2 depending on graph and infection size
+USPG_Hetero_30_Time=list()
 
 k=1
-while(k<=50){ #Generate 100 infection graphs
+while(k<=200){ #Generate 100 infection graphs
 	print(k)
-random=as.numeric(c((sample(V(graph))[1]))) #Randomly pick one source
+USPG=as.numeric(c((sample(V(graph))[1]))) #USPGly pick one source
 
 t=0
 neighbors=list()
@@ -20,7 +20,7 @@ temp_active=list()
 perm_active=list()
 temp=list()
 adj=list()
-perm_active=random
+perm_active=USPG
 time=list()
 tt=0
 time=tt
@@ -96,6 +96,10 @@ while(1)
 			time[length(unlist(perm_active))+1]=as.numeric(paste(tt,ttt,sep="."))
 			perm_active[length(unlist(perm_active))+1]=(inactive)
 			
+			if(length(unlist(perm_active))>=length(V(graph))*0.3)
+			{
+				break
+			}
 			#print(unlist(perm_active))				
 			#print(prob)
 		}
@@ -103,8 +107,8 @@ while(1)
 	if(length(unlist(perm_active))>=length(V(graph))*0.25) #Set infection size
 			{
 				if(length(unlist(perm_active))<=length(V(graph))*0.35){
-				Random_Hetero_30[[length(Random_Hetero_30)+1]]=unlist(perm_active)
-				Random_Hetero_30_Time[[length(Random_Hetero_30_Time)+1]]=unlist(time)
+				USPG_Hetero_30[[length(USPG_Hetero_30)+1]]=unlist(perm_active)
+				USPG_Hetero_30_Time[[length(USPG_Hetero_30_Time)+1]]=unlist(time)
 				print(unlist(perm_active))	
 				k=k+1
 				break
@@ -121,16 +125,16 @@ while(1)
 }
 }
 
-# save(Random_Hetero_30, file="./200_t/Random_Hetero_30.1.RData")
-# save(Random_Hetero_30_Time, file="./200_t/Random_Hetero_30_Time.1.RData")
+# save(USPG_Hetero_30, file="./200_t/USPG_Hetero_30.1.RData")
+# save(USPG_Hetero_30_Time, file="./200_t/USPG_Hetero_30_Time.1.RData")
 
-# load("./200_t/Random_Hetero_30.4.RData")
-# load("./200_t/Random_Hetero_30_Time.4.RData")
-# f4 <- Random_Hetero_30
-# t4 <- Random_Hetero_30_Time
+# load("./200_t/USPG_Hetero_30.4.RData")
+# load("./200_t/USPG_Hetero_30_Time.4.RData")
+# f4 <- USPG_Hetero_30
+# t4 <- USPG_Hetero_30_Time
 
-# Random_Hetero_30 <- c(f1,f2,f3,f4)
-# Random_Hetero_30_Time <- c(t1,t2,t3,t4)
+# USPG_Hetero_30 <- c(f1,f2,f3,f4)
+# USPG_Hetero_30_Time <- c(t1,t2,t3,t4)
 
-# save(Random_Hetero_30, file="./200_t/Random_Hetero_30.RData")
-# save(Random_Hetero_30_Time, file="./200_t/Random_Hetero_30_Time.RData")
+# save(USPG_Hetero_30, file="./200_t/USPG_Hetero_30.RData")
+# save(USPG_Hetero_30_Time, file="./200_t/USPG_Hetero_30_Time.RData")
